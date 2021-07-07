cee7f03fb88a273c9976163b5a86f5b1fa0ab58d
Ciprian <cozma_ciprian19@yahoo.com>
2021-07-07 21:06:05 +0300

    added if/else

--- compareBMIs.js
+++ compareBMIs.js
@@ -8,11 +8,14 @@ const calculateBMI = (mass, height) => {
 const bmiMark = calculateBMI(78, 1.69);
 const bmiJohn = calculateBMI(92, 1.95);
 
-const bmiMark2 = calculateBMI(95, 1.88);
-const bmiJohn2 = calculateBMI(85, 1.76);
+// const bmiMark2 = calculateBMI(95, 1.88);
+// const bmiJohn2 = calculateBMI(85, 1.76);
 
 const markHigherBMI = bmiMark > bmiJohn;
-const markHigherBMI2 = bmiMark2 > bmiJohn2;
+// const markHigherBMI2 = bmiMark2 > bmiJohn2;
 
-console.log(markHigherBMI)
-console.log(markHigherBMI2)
+if(markHigherBMI) {
+    console.log(`Mark's BMI is higher tha John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+}
